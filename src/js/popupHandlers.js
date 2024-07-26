dbe19322e75f0dc6cb820c8f3553fab4b5a3eb31
Alexander Poltavskiy <yuhcha2@yandex.ru>
2024-07-26 15:06:40 +0400

    fix: added minor fixes

--- src/js/popupHandlers.js
+++ src/js/popupHandlers.js
@@ -3,11 +3,13 @@ const popupElements = [
   { 'btn': '.menu__background', 'block': '.menu' },
   { 'btn': '.menu__close', 'block': '.menu' },
   { 'btn': '.button--call', 'block': '.callback--call' },
+  { 'btn': '.button--call-menu', 'block': '.callback--call' },
   { 'btn': '.callback__close--call', 'block': '.callback--call' },
   { 'btn': '.callback__background--call', 'block': '.callback--call' },
   { 'btn': '.button--form', 'block': '.callback--form' },
   { 'btn': '.callback__close--form', 'block': '.callback--form' },
-  { 'btn': '.callback__background--form', 'block': '.callback--form' }
+  { 'btn': '.callback__background--form', 'block': '.callback--form' },
+  { 'btn': '.button--form-menu', 'block': '.callback--form' }
 ]
 
 popupElements.forEach(function (popup) {
